c34a06c8f8fd7e08933d1fdf7b8db98fb8c021da
ricardotavares <cruzcastro07@gmail.com>
2020-08-30 00:14:13 +0100

    feat: add indicator filter

---
+++ application/src/Filter/IndicatorFilter.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Filter;
+
+use App\Filter\Interfaces\CategoryValidator;
+use App\Filter\Interfaces\IndicatorFilterInterface;
+use App\Filter\Interfaces\SubThemeValidator;
+
+class IndicatorFilter implements IndicatorFilterInterface
+{
+    /**
+     * @param array $theme
+     * @param array $indicatorIds
+     * @return MatchData|null
+     */
+    public function applyFilter(array $theme, array $indicatorIds): ?MatchData
+    {
+        $categoryValidator = new CategoryValidator();
+        $subThemeValidator = new SubThemeValidator();
+        $indicatorValidator = new IndicatorValidator();
+
+        $categoryValidator->setNextValidator($indicatorValidator);
+        $subThemeValidator->setNextValidator($categoryValidator);
+
+        return $subThemeValidator->apply($theme, $indicatorIds);
+    }
+}
